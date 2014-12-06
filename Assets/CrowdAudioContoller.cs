dec0b2791e7ea065d0e702e5f29771029fa9e8c0
Carl Hughes <devilqube42@gmail.com>
2014-12-06 18:55:43 +0000

    start of crowd Audio

---
+++ Assets/CrowdAudioContoller.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdAudioContoller : MonoBehaviour
+{
+
+    public AudioSource source;
+
+    public AudioSource voiceSource;
+
+    public AudioClip[] crowdRandoms;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+        source = GetComponent<AudioSource>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator queueCrowd()
+    {
+        yield return new WaitForSeconds(Random.Range(0.0f, 1.5f));
+
+        //source.clip=="";
+        source.pitch = Random.Range(0.7f, 1.3f);
+        source.loop = true;
+        source.Play();
+    }
+
+
+    void StartCrowd()
+    {
+        source.pitch = Random.Range(0.7f, 1.3f);
+        source.loop = true;
+        source.Play();
+
+
+        StartCoroutine("queueCrowd)");
+    }
+}
