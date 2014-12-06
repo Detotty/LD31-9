c71fae6d4a7c8878178cea3f02077fcf4d07600f
Gareth Williams <gareth@garethw.net>
2014-12-06 14:46:31 +0000

    Beginning of things
    - Basic arena layout
    - Torch/lighting prefab
    - Kid entity/prefab with basic controls
    - Weapon attachment + animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public Transform HUD;
+
+	// "Physics"
+    public float walkSpeed = 0.000001f;
+    public float SpeedLimit = 1f;
+    public float Speed = 10;
+
+    public Transform Sprite;
+
+    // States
+    
+
+    // Stats
+    public Dictionary<string, AudioSource> Sounds = new Dictionary<string, AudioSource>();  
+
+    
+
+    private float turntarget = 12f;
+    private Vector2 actualSize = new Vector2(5f,5f);
+
+    private float fstepTime = 0f;
+
+    private tk2dSpriteAnimator spriteAnim;
+
+    void Awake()
+    {
+        turntarget = actualSize.x;
+
+        //anim = Sprite.GetComponent<tk2dSpriteAnimator>();
+
+        GameObject soundsObject = transform.FindChild("Audio").gameObject;
+        foreach (AudioSource a in soundsObject.GetComponents<AudioSource>())
+        {
+            Sounds.Add(a.clip.name, a);
+        }
+    }
+
+    void Update()
+    {
+        //Input
+        float h = Input.GetAxis("P1 Horizontal");
+        float v = Input.GetAxis("P1 Vertical");
+
+        if (h > 0f) turntarget = actualSize.x;
+        if (h < 0f) turntarget = -actualSize.x;
+
+        Speed = walkSpeed;
+        rigidbody.velocity = transform.TransformDirection(new Vector3(h, 0, v).normalized) * Speed;
+
+        if (rigidbody.velocity.magnitude > 0f)
+        {
+            fstepTime += Time.deltaTime;
+            if (fstepTime > 0.1f)
+            {
+                //Sounds["fstep"].Play();
+                fstepTime = 0f;
+            }
+        }
+
+        //if (!anim.IsPlaying("MonsterLadderTransferOn") && !anim.IsPlaying("MonsterLadderTransferOff"))
+        //{
+        //    if (rigidbody.velocity.magnitude > 0f) anim.Play("MonsterWalk");
+        //    else anim.Play("MonsterIdle");
+        //}
+     
+        Sprite.localScale = Vector3.Lerp(Sprite.transform.localScale, new Vector3(turntarget, actualSize.y, 1f), 0.25f);
+
+        if (Input.GetButtonDown("P1 Weapon"))
+        {
+            transform.FindChild("Weapon_Swipe").GetComponent<Animation>().Play("Weapon_Swipe");
+        }
+
+    }
+
+    
+
+  
+}
---
+++ Assets/Torch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour
+{
+
+    public float MaxIntensity = 3f;
+    public float MinIntensity = 0f;
+
+    public float TargetIntensity;
+    private float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    GetComponent<Light>().intensity = Random.Range(MaxIntensity, MinIntensity);
+
+        NewIntensity();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, TargetIntensity,speed);
+
+	    if (Mathf.Abs(GetComponent<Light>().intensity - TargetIntensity) < 0.025f) NewIntensity();
+
+	}
+
+    void NewIntensity()
+    {
+        TargetIntensity = Random.Range(MaxIntensity, MinIntensity);
+        speed = Random.Range(0.2f, 0.3f);
+    }
+}
