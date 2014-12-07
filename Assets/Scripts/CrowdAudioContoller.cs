8e3f699020f9560f3d4d30cf7bbb1066acd5d121
Carl Hughes <devilqube42@gmail.com>
2014-12-07 00:23:43 +0000

    Crowd Audio item drop audio added

---
+++ Assets/Scripts/CrowdAudioContoller.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdAudioContoller : MonoBehaviour
+{
+
+    public AudioSource source;
+
+    public AudioSource voiceSource;
+
+    public AudioClip[] crowdRandoms;
+    public bool backgroundNoise;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (source != null && voiceSource != null)
+        {
+            StartCrowd();
+        }
+       
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator QueueCrowd()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 5.0f));
+
+        voiceSource.clip = crowdRandoms[Random.Range(0, crowdRandoms.Length)];
+        source.pitch = Random.Range(0.5f, 1.3f);
+        Debug.Log("Playing Random");
+        voiceSource.Play();
+        StartCoroutine("QueueCrowd");
+    }
+
+
+    void StartCrowd()
+    {
+       // source.pitch = Random.Range(0.7f, 1.3f);
+        source.loop = true;
+
+
+        if (backgroundNoise)
+        {
+            source.Play();
+        }
+
+
+        StartCoroutine("QueueCrowd");
+    }
+}
---
+++ Assets/Scripts/ItemAudioEventController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemAudioEventController : MonoBehaviour {
+
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+
+        source = GetComponent<AudioSource>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Playing Item Event Audio - Trigger");
+        if (other.gameObject.name.Contains("Chunk"))
+        {
+
+
+
+            source.Play();
+
+        }
+    }
+}
