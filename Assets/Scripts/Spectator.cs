d81dd56cb5a4c6ae854eba5e485b31896a38c6d1
Carl Hughes <devilqube42@gmail.com>
2014-12-07 15:03:34 +0000

    Spectators updated to elves

--- Assets/Scripts/Spectator.cs
+++ Assets/Scripts/Spectator.cs
@@ -6,20 +6,41 @@ public class Spectator : MonoBehaviour
 
     public float ThrowTime = 1f;
 
+    public bool enableThrow=false;
+
     private float currentThrowTime = 0f;
 
     private Transform arenaCenter;
 
+    public Transform Sprite;
+    private float turntarget = 5f;
+    
+
 	// Use this for initialization
 	void Start () {
         arenaCenter = GameObject.Find("Arena").transform.FindChild("Center");
+
+        if (arenaCenter.transform.position.x > transform.position.x)
+        {
+            turntarget = 5;
+           
+        }
+        if (arenaCenter.transform.position.x < transform.position.x)
+        {
+            turntarget = -5;
+           
+        }
+
+        Sprite.localScale =  new Vector3(turntarget, 5, 1f);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	    currentThrowTime += Time.deltaTime;
-	    if (currentThrowTime >= ThrowTime)
+	    if (currentThrowTime >= ThrowTime&& enableThrow)
 	    {
 	        currentThrowTime = 0f;
 	        if (Random.Range(0, 10) == 0)
@@ -29,8 +50,8 @@ public class Spectator : MonoBehaviour
 	            {
 	                i.transform.position = transform.position;
 	                Vector3 throwVelocity = ((arenaCenter.position + (Random.insideUnitSphere*7f)) - transform.position);
-	                throwVelocity *= 0.6f;
-	                throwVelocity.y = 6f;
+	               // throwVelocity *= 0.6f;
+	                throwVelocity.y = 7f;
                     i.rigidbody.velocity = throwVelocity;
 	            }
 	        }
